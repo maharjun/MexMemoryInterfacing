2216edf6603d523b57cb4b6029ac1d70c2f156f6
maharjun <arjun210493@gmail.com>
2015-10-20 23:24:29 +0530

    Minor: Removed Useless Comment from MexMem.cpp
    Removed the comment // size_t(3) << 29 from MexMem.cpp
    which is a remnant of (lazily) changing code.

--- Headers/MexMem.cpp
+++ Headers/MexMem.cpp
@@ -1,6 +1,6 @@
 #include "MexMem.hpp"
 
-size_t MemCounter::MemUsageLimitVal = 0xFFFFFFFFFFFFFFFF; //(size_t(3) << 29)
+size_t MemCounter::MemUsageLimitVal = 0xFFFFFFFFFFFFFFFF;
 const size_t & MemCounter::MemUsageLimit = MemCounter::MemUsageLimitVal;
 size_t MemCounter::AccountOpeningKey = 0;
 size_t MemCounter::MemUsageCount = 0;
