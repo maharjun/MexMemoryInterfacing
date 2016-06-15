5c66ef3fa1a8bd65e410f59c6111f9ab82b7796a
Arjun Rao <arjun210493@gmail.com>
2016-06-15 21:10:10 +0530

    Updated VectTreeInfo.hpp
    2.  Removed all usages of int and converted them into int32
    3.  Refactored template programming in VectTreeInfo.hpp to use a
        wrapper class and decay<T>

--- Headers/FlatVectTree/VectTreeInfo.hpp
+++ Headers/FlatVectTree/VectTreeInfo.hpp
@@ -4,43 +4,23 @@
 #include "../MexMem.hpp"
 
 // Code to get information of a particular Nested MexVector Type
-template <typename T, typename B = void> struct getTreeInfo
+template <typename T, typename B = void> struct getTreeInfoBasic
 {
-	static const int depth = -1;
+	static const uint32_t depth = uint32_t(-1);
 };
 
-// This is a specialization that removes const, volatile and references from types
-template <typename T> 
-struct getTreeInfo<
-		T, 
-		typename std::enable_if <
-			std::is_const<T>::value || 
-			std::is_volatile<T>::value || 
-			std::is_reference<T>::value
-		>::type
-	>
-	{
-	static constexpr int depth = getTreeInfo<
-		typename std::remove_reference<
-			typename std::remove_cv<T>::type
-		>::type
-	>::depth;
-	typedef
-        typename getTreeInfo<
-            typename std::remove_reference<
-                typename std::remove_cv<T>::type
-            >::type
-        >::type
-        type;
-};
-
-template <typename T, class Al> struct getTreeInfo<MexVector<T, Al> > {
-	static constexpr int depth = 0;
+template <typename T, class Al> struct getTreeInfoBasic<MexVector<T, Al> > {
+	static constexpr uint32_t depth = 0;
 	typedef T type;
 };
-template <typename T, class Al> struct getTreeInfo<MexVector<MexVector<T, Al> > > {
-	static constexpr int depth = getTreeInfo<MexVector<T> >::depth + 1;
-	typedef typename getTreeInfo<MexVector<T> >::type type;
+
+template <typename T, class Al> struct getTreeInfoBasic<MexVector<MexVector<T, Al> > > {
+	static constexpr uint32_t depth = getTreeInfoBasic<MexVector<T> >::depth + 1;
+	typedef typename getTreeInfoBasic<MexVector<T> >::type type;
 };
+
+// This is a wrapper that removes const, volatile and references from types
+template <typename T>
+struct getTreeInfo : public getTreeInfoBasic<typename std::decay<T>::type> {};
 
 #endif
